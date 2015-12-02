8b2d4aa920d865bb049d4e129f6bce2c5a1a1874
edelmotte <etienne.delmotte@efrei.net>
2015-12-02 10:15:23 +0100

    Page d'accueil

--- website/urls.py
+++ website/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^orange/', include('orange.urls')),
+    url(r'^', include('orange.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
